47b29fd18cb7d02dea3d876dcac0e838dfe93577
m8sec <m8r0wn@protonmail.com>
2024-10-29 21:02:07 -0400

    updates

---
+++ docs/assets/js/data/swconf.js
@@ -0,0 +1,38 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1730250072',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/tools/',
+      
+        '/contact/',
+      
+        '/archives/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+
+      
+      
+        '/assets/js/dist/categories.min.js',
+        '/assets/js/dist/commons.min.js',
+        '/assets/js/dist/home.min.js',
+        '/assets/js/dist/misc.min.js',
+        '/assets/js/dist/page.min.js',
+        '/assets/js/dist/post.min.js'
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
